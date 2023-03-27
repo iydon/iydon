09817bd0e0c58411ccaa2f5682ecd61cb5d2e070
Iydon Liang <liangiydon@gmail.com>
2023-03-27 18:21:27 +0800

    :sparkles: Add glhf::{end, silver_bullet}

---
+++ iydon/glhf/silver_bullet.py
@@ -0,0 +1,13 @@
+'''No Silver Bullet.
+
+Examples:
+    >>> from iydon.glhf import silver_bullet
+    Traceback (most recent call last):
+        ...
+    SyntaxError: No Silver Bullet.
+
+Raises:
+    SyntaxError: All times.
+'''
+
+raise SyntaxError('No Silver Bullet.')
